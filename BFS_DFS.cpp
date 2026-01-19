28b52ba6e1367beb981ea5fe0055f03b5885a4fb
dominikamac758 <dominika06maciejewska@gmail.com>
2026-01-19 19:52:21 +0100

    Update BFS_DFS.cpp

--- BFS_DFS.cpp
+++ BFS_DFS.cpp
@@ -1,1 +1,24 @@
+DFS- inorder (:
+
+void dfs(Node* root) {
+    if (root == nullptr) return;
+
+    dfs(root->left);
+    cout << root->value << " ";
+    dfs(root->right);
+}
+
+DFS-preorder (korzen->lewy->prawy):
+
+void dfs_preorder(Node* root) {
+  if (root == nullptr) return;
+
+  cout << root->value << " ";
+  dfs_preorder(root->left);
+  dfs_preorder(root->right);
+}
+
+DFS-postorder (
+BFS:
+
 
